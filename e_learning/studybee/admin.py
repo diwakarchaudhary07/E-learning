b0242c71eec74ea59879f94663b44eade20c692e
diwakar chaudhary <boyroyal4853@gmail.com>
2026-09-01 23:40:25 +0530

    add

--- e_learning/studybee/admin.py
+++ e_learning/studybee/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AboutUs, Article, ContactMessage, Course, CustomUser, LiveClass, LiveClassResource, OtpVerification
+from .models import AboutUs, Article, ChatMessage, ContactMessage, Course, CustomUser, LiveClass, LiveClassResource, OtpVerification
 
 
 @admin.register(CustomUser)
@@ -10,9 +10,10 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_filter = ('is_staff', 'is_active', 'gender')
     fieldsets = (
         ('Account', {'fields': ('username', 'email', 'password')}),
-        ('Profile', {'fields': ('full_name', 'mobile_no', 'dob', 'gender', 'profile_image', 'address', 'alternate_mobile_no')}),
+        ('Profile', {'fields': ('full_name', 'mobile_no', 'dob', 'gender', 'profile_image', 'address', 'alternate_mobile_no', 'is_email_verified')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
+    readonly_fields = ('date_joined', 'last_login')
 
 
 @admin.register(ContactMessage)
@@ -28,6 +29,7 @@ class OtpVerificationAdmin(admin.ModelAdmin):
     list_display = ('user', 'otp_code', 'created_at', 'expires_at', 'is_used')
     readonly_fields = ('created_at', 'expires_at')
     search_fields = ('user__username', 'user__email', 'otp_code')
+    list_filter = ('is_used', 'created_at')
 
 
 @admin.register(Course)
@@ -37,6 +39,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ('category', 'language', 'created_at')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at', 'slug')
 
 
 @admin.register(AboutUs)
@@ -52,7 +55,9 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'author', 'published_at', 'reading_time')
     prepopulated_fields = {'slug': ('title',)}
     search_fields = ('title', 'category', 'author', 'summary')
+    list_filter = ('category', 'published_at', 'created_at')
     ordering = ('-published_at', '-created_at')
+    readonly_fields = ('published_at', 'created_at', 'updated_at', 'slug')
 
 
 @admin.register(LiveClass)
@@ -61,10 +66,30 @@ class LiveClassAdmin(admin.ModelAdmin):
     list_filter = ('status', 'subject', 'class_date')
     search_fields = ('title', 'subject', 'instructor_name', 'course_name')
     ordering = ('-class_date', '-class_time')
+    readonly_fields = ('created_at', 'updated_at')
 
 
 @admin.register(LiveClassResource)
 class LiveClassResourceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'live_class', 'resource_type')
-    list_filter = ('resource_type',)
+    list_display = ('title', 'live_class', 'resource_type', 'created_at')
+    list_filter = ('resource_type', 'created_at')
     search_fields = ('title', 'live_class__title')
+    readonly_fields = ('created_at',)
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = ('get_user_name', 'get_message_preview', 'created_at')
+    search_fields = ('user__full_name', 'user__email', 'user_message', 'bot_response')
+    list_filter = ('created_at',)
+    readonly_fields = ('created_at', 'user_message', 'bot_response')
+    ordering = ('-created_at',)
+
+    def get_user_name(self, obj):
+        return obj.user.full_name if obj.user else 'Guest'
+    get_user_name.short_description = 'User'
+
+    def get_message_preview(self, obj):
+        preview = obj.user_message[:50]
+        return f"{preview}..." if len(obj.user_message) > 50 else preview
+    get_message_preview.short_description = 'Message Preview'
